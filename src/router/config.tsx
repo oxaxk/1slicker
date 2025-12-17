eb53d15e16f4338f7a2f02f866ea27169b4bf9b1
oxaxk <onurardakck@icloud.com>
2025-12-17 01:08:17 +0100

    Update config.tsx

--- src/router/config.tsx
+++ src/router/config.tsx
@@ -1,37 +1,53 @@
 // File: src/router/config.tsx
 import React from 'react';
-import { createBrowserRouter, RouteObject, Navigate } from 'react-router-dom';
+import { createBrowserRouter, RouteObject, Navigate, Outlet } from 'react-router-dom';
 
 import NotFound from '../pages/NotFound';
 import Home from '../pages/home/page';
 import Datenschutz from '../pages/datenschutz/page';
 import Impressum from '../pages/impressum/page';
 import CookieSettingsPage from '../pages/cookie/page';
+import CookieBanner from '../components/feature/CookieBanner';
+
+function RootLayout() {
+  return (
+    <>
+      <Outlet />
+      <CookieBanner />
+    </>
+  );
+}
 
 const routes: RouteObject[] = [
   {
     path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/home',
-    element: <Navigate to="/" replace />,
-  },
-  {
-    path: '/cookie',
-    element: <CookieSettingsPage />,
-  },
-  {
-    path: '/datenschutz',
-    element: <Datenschutz />,
-  },
-  {
-    path: '/impressum',
-    element: <Impressum />,
-  },
-  {
-    path: '*',
-    element: <NotFound />,
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'home',
+        element: <Navigate to="/" replace />,
+      },
+      {
+        path: 'cookie',
+        element: <CookieSettingsPage />,
+      },
+      {
+        path: 'datenschutz',
+        element: <Datenschutz />,
+      },
+      {
+        path: 'impressum',
+        element: <Impressum />,
+      },
+      {
+        path: '*',
+        element: <NotFound />,
+      },
+    ],
   },
 ];
 
