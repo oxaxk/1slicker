10559a6a3009e2bd19f0ec96562003bf2e71d9ba
oxaxk <onurardakck@icloud.com>
2025-12-20 22:06:01 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './router/config';
+import CookieBanner from './components/feature/CookieBanner';
 
 function App() {
   return (
     <>
       <RouterProvider router={router} />
+      <CookieBanner />
     </>
   );
 }
